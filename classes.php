50fa921288fcfd8c276ecc96e4e4a270af2dd266
BowenHuang80 <bowenhuang80@hotmail.com>
2014-10-08 20:24:08 -0400

    v1.00
    Test

---
+++ classes.php
@@ -0,0 +1,24 @@
+<?php
+    
+    /* demonstrate class in PHP */
+    class FormValidation {
+               
+        /* check if a string is empty */
+        function CheckEmptyField($tgtString) {
+        }
+        /* 
+        function CheckString($tgtStr) {
+
+        }
+
+        /* check the number and length */
+        function CheckNumber($tgtNum, $length) {
+
+        }
+
+        function CheckOption($tgtOpt, $optArray) {
+
+        }
+        
+    }
+?>
